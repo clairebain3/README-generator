f002ed633946f4be0c92ada19d928276bc38ae3f
Claire Bain <claire.bain3@gmail.com>
2022-11-30 18:27:54 -0800

    adds inquire questions to generate readme

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = ['What is your project title?','What is your project description?', 'What are the installation instructions?', 'What is the usage information?', 'What are the contribution guidelines?', 'What are the test instructions?'];
@@ -7,7 +9,52 @@ const questions = ['What is your project title?','What is your project descripti
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+inquirer
+.prompt([
+    {
+    type: 'input',
+    name: 'title',
+    message: questions[0],
+
+},
+{
+    type: 'input',
+    name: 'description',
+    message: questions[1],
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: questions[2],
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: questions[3],
+},
+{
+    type: 'input',
+    name: 'contribution',
+    message: questions[4],
+},
+{
+    type: 'input',
+    name: 'test',
+    message: questions[5],
+}
+
+])
+.then((answers)=> {
+    const htmlPageContent = generateHTML(answers);
+    fs.writeFile(README.md), htmlPageContent, (err) =>
+    err ? console.log(err) : console.log('All set!')
+}
+
+)
+
+
+}
 
 // Function call to initialize app
 init();
